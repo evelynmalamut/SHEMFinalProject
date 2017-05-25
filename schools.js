d3c4068f3c35b8346dde609e1785889427d270d5
samwallan <samuelwallan@students.berkeley.net>
2017-05-24 21:36:42 -0700

    fixed some comma issues

--- schools.js
+++ schools.js
@@ -12,7 +12,7 @@ $(document).ready( function() {
         var configProfile = {
             "profile": {"screenName": word},
             "domId": 'example1',
-            "maxTweets": 1,
+            "maxTweets": 4,
             "enableLinks": true,
             "showUser": true,
             "showTime": true,
@@ -25,7 +25,7 @@ $(document).ready( function() {
 
         console.log(allTweetText);
 
-        setTimeout(function(){ToneAnalizer(allTweetText)}, 500);
+        setTimeout(function(){ToneAnalizer(allTweetText)}, 1000);
 
 
 
@@ -51,37 +51,57 @@ function ToneAnalizer(array){
     var textdext = "";
     var textdext2= "";
     varx = [];
-    for(var i = 0; i<array.length; i++){
-        var tempHolder="";
-        console.log(array[i])
+    for(var i = 0; i<array.length; i++) {
+        var tempHolder = "";
+        console.log(array[i].replace(",",""))
         array[i].replace(/[^a-zA-Z0-9]/g, '')
-        console.log(array[i])
-        for(var j = 0; j<array[i].length; j++){
+        console.log(array[i].replace(",",""))
+        for (var j = 0; j < array[i].length; j++) {
 
-            if(array[i].charAt(j)!==" "){
-                tempHolder = tempHolder+ array[i].charAt(j);
-            }else if(array[i].charAt(j)==" ") {
-                tempHolder = tempHolder+ "%20";
+            if (hasHTML(array[i])) {
+
+                if (array[i].charAt(j) !== " ") {
+                    tempHolder = tempHolder + array[i].charAt(j);
+                } else if (array[i].charAt(j) == " ") {
+                    tempHolder = tempHolder + "%20";
+                }
             }
-            }
-        varx[i] = tempHolder;
+            varx[i] = tempHolder.replace(",","");
         }
 
-        for(var i =0; i<varx.length; i++){
+        for (var i = 0; i < varx.length; i++) {
             textdext = textdext + varx[i];
         }
 
-        for(var i = 0; i<varx.length; i++){
-            textdext2 = textdext2 + varx[i] +"%2C";
+        for (var i = 0; i < varx.length; i++) {
+            textdext2 = textdext2 + varx[i] + "%2C";
         }
-//console.log(textdext)
-   // console.log(textdext2)
+        textdext.replace('undefined','');
+        textdext2.replace('undefined','');
+    }
+
+    textdext2 = textdext2.substr(0, textdext2.length -3)
+
+    textdext2.replace(/[^\w\s!?]/g,'');
+    textdext.replace(/[^\w\s!?]/g,'');
+
+
+    textdext2.replace(",","");
+    textdext.replace(",","");
+console.log(textdext)
+    console.log(textdext2)
 
 var searchQuery = "https://watson-api-explorer.mybluemix.net/natural-language-understanding/api/v1/analyze?version=2017-02-27&text="+textdext+"&features=sentiment&return_analyzed_text=false&clean=true&fallback_to_raw=true&concepts.limit=8&emotion.document=true&entities.limit=50&entities.emotion=false&entities.sentiment=false&keywords.limit=50&keywords.emotion=false&keywords.sentiment=false&relations.model=en-news&semantic_roles.limit=50&semantic_roles.entities=false&semantic_roles.keywords=false&sentiment.document=true&sentiment.targets="+textdext2;
 
-    searchQuery.replace(/[^a-zA-Z ]/g, "")
+   // searchQuery//.replace(/[^\w\s!?]/g,'');
     console.log(searchQuery)
     }
 
-
+function hasHTML(text){
+    for(var x =0; x<text.length; x=x+1){
+        if(text.substr(x,x+4)== 'html'){
+        return false}
+    }
+    return true
+};
 
