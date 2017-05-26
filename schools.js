41229da78c3d88ca4c8b6be304c3d94aa328c2ff
samwallan <samuelwallan@students.berkeley.net>
2017-05-25 19:48:37 -0700

    stuff

--- schools.js
+++ schools.js
@@ -15,7 +15,7 @@ $(document).ready( function() {
             /*
             this is the number of tweets that it'll get
              */
-            "maxTweets": 6,
+            "maxTweets": 5,
             "enableLinks": true,
             "showUser": true,
             "showTime": true,
@@ -40,46 +40,65 @@ $(document).ready( function() {
     goes back to page 1 with the back button
      */
     $("#myButton2").click(function () {
-        $("body").pagecontainer("change", "#page1", {transition: "fade"});
+        $("body").pagecontainer("change", "#page1", {transition: "slide"});
     });
 });
 
 
-var arrayOfSent = []
-function ToneAnalizer(tweetList){
-    for (var i = 0; i < tweetList.length; i= i+1) {
+var arrayOfSent = [];
+function ToneAnalizer(tweetList) {
+    for (var i = 0; i < tweetList.length; i = i + 1) {
         console.log(tweetList[i])
-            var tempVar = tweetList[i].replace(/[^a-zA-Z0-9 ]/g, '');
-            $.ajax({
-                url: "https://watson-api-explorer.mybluemix.net/natural-language-understanding/api/v1/analyze?version=2017-02-27&text=" + tempVar + "&features=sentiment&return_analyzed_text=true&clean=true&fallback_to_raw=true&concepts.limit=8&emotion.document=true&entities.limit=50&entities.emotion=false&entities.sentiment=false&keywords.limit=50&keywords.emotion=false&keywords.sentiment=false&relations.model=en-news&semantic_roles.limit=50&semantic_roles.entities=false&semantic_roles.keywords=false&sentiment.document=true",
-                type: 'GET',
-                crossDomain: true,
-                dataType: 'json',
-                success: function (result) {
-                 console.log(result.sentiment.document.score,result.analyzed_text)
-                    arraypush(result.sentiment.document.score);
-                   // arrayOfSent.push(result.sentiment.document.score)
+        var tempVar = tweetList[i].replace(/[^a-zA-Z0-9 ]/g, '');
+        $.ajax({
+            url: "https://watson-api-explorer.mybluemix.net/natural-language-understanding/api/v1/analyze?version=2017-02-27&text=" + tempVar + "&features=sentiment&return_analyzed_text=true&clean=true&fallback_to_raw=true&concepts.limit=8&emotion.document=true&entities.limit=50&entities.emotion=false&entities.sentiment=false&keywords.limit=50&keywords.emotion=false&keywords.sentiment=false&relations.model=en-news&semantic_roles.limit=50&semantic_roles.entities=false&semantic_roles.keywords=false&sentiment.document=true",
+            type: 'GET',
+            crossDomain: true,
+            dataType: 'json',
+            success: function (result) {
+                console.log( result.sentiment.document.score)
+                $("#example2").append(" <li class='ui-li-has-alt ui-first-child' ><a href='' class='ui-btn'><h3>" + result.sentiment.document.score + "</h3><p >" + result.analyzed_text + "</p></a></li>")
+                arraypush(result.sentiment.document.score);
 
-                },
-                error: function () {
-                    console.log('Failed!');
-                }
-            })
+            },
+            error: function () {
+                console.log('Failed!');
+            }
+        });
 
 
-    console.log(arrayOfSent)
+
+    }
+    console.log(arrayOfSent);
+    setTimeout(function(){Averager(arrayOfSent)}, 1000)
 
 }
-
 
 function arraypush(score){
     arrayOfSent.push(score)
     console.log(arrayOfSent)
 }
 
-
-
+function Averager (tweetScores){
+    var overalsent= 0
+    var overalsenttext= ""
+    for (i = 0; i < tweetScores.length; i++) {
+        overalsent = overalsent +tweetScores[i];
     }
+    overalsent = overalsent / tweetScores.length;
+    if (overalsent > 0) {
+        overalsenttext = "Positive"
+    } else {
+        overalsenttext = "Negative"
+    }
+    $("#values").text(overalsenttext + " " + overalsent)
+
+
+}
+
+
+
+
 
 
 
